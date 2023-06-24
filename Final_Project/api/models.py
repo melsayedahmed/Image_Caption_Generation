5c7db50537de6bc87f34cfac5d5afe2d4d82a4a6
melsayedahmed <mohab.moh7000@gmail.com>
2023-06-24 06:55:05 +0300

    update AI model

--- Final_Project/api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class ApiImage(models.Model):
-    name = models.TextField()
-    Main_Img = models.ImageField(upload_to='images/')
-
-    def __str__(self):
-        return self.name
